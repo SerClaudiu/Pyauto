5e2d1347078f47cda04cfd7fb9cb6a0c07a9db29
Serghei Claudiu <claudiuserghei@gmail.com>
2024-02-29 14:13:08 +0200

    adaugat222

---
+++ main.py
@@ -0,0 +1,18 @@
+import pyautogui, sys
+# print('Press Ctrl-C to quit.')
+# try:
+#     while True:
+#         x, y = pyautogui.position()
+#         positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
+#         print(positionStr, end='')
+#         print('\b' * len(positionStr), end='', flush=True)
+# except KeyboardInterrupt:
+#     print('\n')
+
+
+#pyautogui.moveTo(X,Y, NR SECUNDE)
+
+scratches="201,215"
+
+#pyautogui.moveTo(201,215, 3)
+pyautogui.click(201,215, 2)
